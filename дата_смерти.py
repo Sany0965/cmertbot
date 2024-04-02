9f470e7bf0af20d71c310688739bfeba4906f33e
pizzaway <100470077+Sany0965@users.noreply.github.com>
2024-04-02 12:07:27 +0300

    Update дата_смерти.py

--- дата_смерти.py
+++ дата_смерти.py
@@ -3,91 +3,70 @@ import datetime
 import random
 import time
 
-bot = telebot.TeleBot('Ваштокен')
+bot = telebot.TeleBot('ТОКЕНОТБОТФАЗЕР')
 
+# Чтение причин смерти из файла
+def read_death_reasons():
+    with open('death_reasons.txt', 'r', encoding='utf-8') as file:
+        reasons = file.readlines()
+    return reasons
+
+# Функция для предсказания даты смерти
+def predict_death(user_name):
+    reasons = read_death_reasons()
+
+    start_date = datetime.datetime(2023, 11, 6)
+    end_date = datetime.datetime(2100, 1, 1)
+    random_date = start_date + datetime.timedelta(seconds=random.randint(0, int((end_date - start_date).total_seconds())))
+
+    today = datetime.datetime.now()
+    delta = random_date - today
+    years, days = divmod(delta.days, 365)
+    months = days // 30
+    days = days % 30
+
+    formatted_date = random_date.strftime("%d %B %Y")
+    time_until_death = f'{years} год(а/лет), {months} месяц(а/ев), {days} день(дней)'
+    
+    random_reason = random.choice(reasons).strip()
+    if ',' in random_reason:
+        reason_index, reason_text = random_reason.split(',')
+        random_reason = reason_text.strip()
+
+    return formatted_date, time_until_death, random_reason
+
+# Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.send_message(message.chat.id, "Привет! Я бот, который предскажет дату вашей смерти. Для предсказания отправьте команду /data_smerti и ваше Имя и Фамилию (Например: Иванов Иван)")
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.row(telebot.types.InlineKeyboardButton('Код бота 🤖', callback_data='code'),
+                telebot.types.InlineKeyboardButton('Предсказать дату смерти 💀', callback_data='predict_death'))
+    bot.send_message(message.chat.id, "Привет! Я бот, который предскажет дату вашей смерти.", reply_markup=markup)
 
-@bot.message_handler(commands=['data_smerti'])
-def predict_death_date(message):
-    try:
-        msg = bot.send_message(message.chat.id, "Пожалуйста, пришлите ваше Имя и Фамилию (Например: Иванов Иван)")
+# Обработчик нажатия на инлайн-кнопки
+@bot.callback_query_handler(func=lambda call: True)
+def callback_handler(call):
+    if call.data == 'code':
+        bot.send_message(call.message.chat.id, "Исходный код бота доступен на GitHub: [GitHub Repo](https://github.com/Sany0965/cmertbot) 📝", parse_mode='Markdown')
+    elif call.data == 'predict_death':
+        msg = bot.send_message(call.message.chat.id, "Пожалуйста, пришлите ваше Имя и Фамилию (Например: Иванов Иван) 🙂")
         bot.register_next_step_handler(msg, process_name)
-    except Exception as e:
-        bot.send_message(message.chat.id, f'Произошла ошибка: {str(e)}')
 
+# Обработчик ввода имени и фамилии
 def process_name(message):
     try:
         user_name = message.text.lower()
-        is_special_user = "ура роблокс" in user_name
 
-        bot.send_message(message.chat.id, 'Подключаюсь к серверам Бога Смерти 🍕...')
-        time.sleep(5)
+        # Предсказание даты смерти
+        formatted_date, time_until_death, random_reason = predict_death(user_name)
 
-        start_date = datetime.datetime(2023, 11, 6)
-        end_date = datetime.datetime(2100, 1, 1)
-        random_date = start_date + datetime.timedelta(seconds=random.randint(0, int((end_date - start_date).total_seconds())))
-
-        today = datetime.datetime.now()
-        delta = random_date - today
-        years, days = divmod(delta.days, 365)
-        months = days // 30
-        days = days % 30
-
-        reasons = [
-            "Из-за автомобильной аварии",
-            "Лучший друг",
-            "Убийца",
-            "Старость",
-            "Смерть на работе",
-            "Болезнь",
-            "Вейп или Сигареты",
-            "Случайное падение кирпича",
-            "Атака медузы",
-            "Волшебное заклинание",
-            "Сердечно-сосудистые заболевания",
-            "Инсульт",
-            "Рак",
-            "Бронхиальная астма",
-            "Пневмония",
-            "Дорожно-транспортные происшествия",
-            "Диабет",
-            "Гепатит и цирроз печени",
-            "Другие инфекционные заболевания",
-            "Хроническая обструктивная болезнь легких (ХОБЛ)",
-            "Суицид",
-            "Ожирение",
-            "Употребление алкоголя",
-            "Онкологические заболевания легких",
-            "Болезнь Альцгеймера и другие деменции",
-            "Гепатоцеллюлярная карцинома",
-            "Мочекаменная болезнь",
-            "Хроническая болезнь почек",
-            "Черепно-мозговые травмы"
-        ]
-        random_reason = random.choice(reasons)
-
-        formatted_date = random_date.strftime("%d %B %Y")
-
-        time_until_death = f'{years} год(а/лет), {months} месяц(а/ев), {days} день(дней)'
-
-        result_message = f'{user_name.title()}, ваша дата Смерти: {formatted_date}\n'
-        result_message += f'Вы Умрёте через: {time_until_death}\n'
-        if is_special_user:
-            result_message += "Причина: Электронные сигареты / Лучший друг\n"
-        else:
-            result_message += f'Причина: {random_reason}\n'
-        result_message += "Приятного дня"
+        result_message = f'{user_name.title()}, ваша дата Смерти: {formatted_date} 💀\n'
+        result_message += f'Вы Умрёте через: {time_until_death} ⏳\n'
+        result_message += f'Причина: {random_reason} 😱\n'
+        result_message += "Приятного дня! ☀️"
         bot.send_message(message.chat.id, result_message)
     except Exception as e:
         bot.send_message(message.chat.id, f'Произошла ошибка: {str(e)}')
 
-@bot.message_handler(commands=['code'])
-def send_code(message):
-    bot.send_message(message.chat.id, "Исходный код бота доступен на GitHub: [GitHub Repo](https://github.com/Sany0965/cmertbot)")
-
 if __name__ == "__main__":
     bot.polling(none_stop=True)
-
-
